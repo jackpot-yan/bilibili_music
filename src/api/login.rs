dc60f4609cc1ff22ccfe1a1a7fd34c33f27fb486
jackpot <wikileaks928@gmail.com>
2024-08-23 18:03:04 +0800

    test actions

--- src/api/login.rs
+++ src/api/login.rs
@@ -5,8 +5,8 @@ use super::dto::Passport;
 pub fn login() -> bool {
     match ureq::get(config::PASSURL).call() {
         Ok(res) => {
-            let DataJson:Passport = res.into_json().unwrap();
-            println!("{:?}", DataJson);
+            let successed:Passport = res.into_json().unwrap();
+            println!("{:?}",  successed);
             true
         }
         Err(err) => {
